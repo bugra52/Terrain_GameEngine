49a5b472df8eaf3e4e9f975aa0180469387a91ef
Bugra Goeruer <bugoerue@stud.hs-heilbronn.de>
2021-06-07 04:27:51 +0200

    Update WeatherManager.cs

--- Assets/EnvironmentScript/Sky/WeatherManager.cs
+++ Assets/EnvironmentScript/Sky/WeatherManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class WeatherManager : MonoBehaviour
 {
 
-    public enum Season { NONE, SPRING, AUTUMN, WINTER }
+    public enum Season { NONE, SPRING, SUMMER, AUTUMN, WINTER }
     public enum Weather { NONE, SUNNY, HOTSUN, RAIN, SNOW }
 
     public Season currentSeason;
@@ -31,12 +31,92 @@ public class WeatherManager : MonoBehaviour
 
     public void ChangeSeason(Season seasonType)
     {
-
+        if (seasonType != this.currentSeason)
+        {
+            switch (seasonType)
+            {
+                case Season.SPRING:
+                    currentSeason = Season.SPRING;
+                    break;
+                case Season.SUMMER:
+                    currentSeason = Season.SUMMER;
+                    break;
+                case Season.AUTUMN:
+                    currentSeason = Season.AUTUMN;
+                    break;
+                case Season.WINTER:
+                    currentSeason = Season.WINTER;
+                    break;
+            }
+        }
     }
 
     public void ChangeWeather(Weather weatherType)
     {
+        if (weatherType != this.currentWeather)
+        {
+            switch (weatherType)
+            {
+                case Weather.SUNNY:
+                    currentWeather = Weather.SUNNY;
+                    break;
+                case Weather.HOTSUN:
+                    currentWeather = Weather.HOTSUN;
+                    break;
+                case Weather.RAIN:
+                    currentWeather = Weather.RAIN;
+                    break;
+                case Weather.SNOW:
+                    currentWeather = Weather.SNOW;
+                    break;
+            }
+        }
+    }
 
+    private void Update()
+    {
+        this.seasonTime -= Time.deltaTime;
+
+        if(this.currentSeason == Season.SPRING)
+        {
+            ChangeWeather(Weather.SUNNY);
+
+            if(this.seasonTime <= 0f)
+            {
+                ChangeSeason(Season.SUMMER);
+                this.seasonTime = this.summerTime;
+            }
+        }
+        if (this.currentSeason == Season.SUMMER)
+        {
+            ChangeWeather(Weather.HOTSUN);
+
+            if (this.seasonTime <= 0f)
+            {
+                ChangeSeason(Season.AUTUMN);
+                this.seasonTime = this.summerTime;
+            }
+        }
+        if (this.currentSeason == Season.AUTUMN)
+        {
+            ChangeWeather(Weather.RAIN);
+
+            if (this.seasonTime <= 0f)
+            {
+                ChangeSeason(Season.WINTER);
+                this.seasonTime = this.summerTime;
+            }
+        }
+        if (this.currentSeason == Season.WINTER)
+        {
+            ChangeWeather(Weather.SNOW);
+
+            if (this.seasonTime <= 0f)
+            {
+                ChangeSeason(Season.SPRING);
+                this.seasonTime = this.summerTime;
+            }
+        }
     }
 
 

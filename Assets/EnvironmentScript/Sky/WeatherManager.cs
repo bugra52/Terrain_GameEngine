616aed8167fc052e4ea0fd6eb810abe1e3531eba
Bugra Goeruer <bugoerue@stud.hs-heilbronn.de>
2021-06-07 04:12:28 +0200

    Update WeatherManager.cs

--- Assets/EnvironmentScript/Sky/WeatherManager.cs
+++ Assets/EnvironmentScript/Sky/WeatherManager.cs
@@ -2,14 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class WeatherManager : MonoBehaviour
 {
 
     public enum Season { NONE, SPRING, AUTUMN, WINTER }
     public enum Weather { NONE, SUNNY, HOTSUN, RAIN, SNOW }
 
     public Season currentSeason;
-    public Weather currentWeathera;
+    public Weather currentWeather;
+
+    [Header("Time Settings")]
+    public float seasonTime;
+    public float springTime;
+    public float summerTime;
+    public float autumnTime;
+    public float winterTime;
+
+    public int currentYear;
+
+    private void Start()
+    {
+        this.currentSeason = Season.SPRING;
+        this.currentWeather = Weather.SUNNY;
+        this.currentYear = 1;
+
+        this.seasonTime = this.springTime;
+    }
+
+    public void ChangeSeason(Season seasonType)
+    {
+
+    }
+
+    public void ChangeWeather(Weather weatherType)
+    {
+
+    }
 
 
 

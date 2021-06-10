cfeee901125fa6f6eb5fc1f5012d9bc5dd134203
Bugra Goeruer <bugoerue@stud.hs-heilbronn.de>
2021-06-10 02:59:41 +0200

    Update DayNightController.cs

--- Assets/EnvironmentScript/Sky/DayNightController.cs
+++ Assets/EnvironmentScript/Sky/DayNightController.cs
@@ -14,7 +14,7 @@ public class DayNightController : MonoBehaviour
     private void Awake()
     {
         targetLight = GameObject.FindGameObjectWithTag("Light");
-        targetLight = GameObject.FindGameObjectWithTag("MainCamera");
+        targetMainCamera = GameObject.FindGameObjectWithTag("MainCamera");
     }
 
     // Start is called before the first frame update
@@ -26,6 +26,43 @@ public class DayNightController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        targetLight.GetComponent<Light>().intensity = dayTimer -= Time.deltaTime * 0.3f;
+        if (!isCycle)
+        {
+            targetLight.GetComponent<Light>().intensity = dayTimer -= Time.deltaTime * 0.01f;
+            if (dayTimer <= 0)
+            {
+                isCycle = true; 
+            }
+        }
+
+        else if (isCycle)
+        {
+            targetLight.GetComponent<Light>().intensity = dayTimer += Time.deltaTime * 0.01f;
+            if (dayTimer >= 1)
+            {
+                isCycle = false;
+            }
+        }
+        ChangeCycle();
+    }
+
+    void ChangeCycle()
+    {
+        if(dayTimer >= 0.9f)
+        {
+            targetMainCamera.GetComponent<Skybox>().material = skys[0];
+        }
+        else if(dayTimer >= 0.5f)
+        {
+            targetMainCamera.GetComponent<Skybox>().material = skys[1];
+        }
+        else if (dayTimer >= 0.3f)
+        {
+            targetMainCamera.GetComponent<Skybox>().material = skys[2];
+        }
+        else if (dayTimer >= 0.1f)
+        {
+            targetMainCamera.GetComponent<Skybox>().material = skys[3];
+        }
     }
 }

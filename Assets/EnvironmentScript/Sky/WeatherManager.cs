776ab65828ed0379f86ec8a33d990f9332bf6d30
Bugra Goeruer <bugoerue@stud.hs-heilbronn.de>
2021-06-14 11:57:08 +0200

    Revert "Revert "Merge branch 'Bugo_Branch' into main""
    This reverts commit 3e0cdb8beb7e2fb01249344bb4d13c932499c964.

--- Assets/EnvironmentScript/Sky/WeatherManager.cs
+++ Assets/EnvironmentScript/Sky/WeatherManager.cs
@@ -116,7 +116,7 @@ public class WeatherManager : MonoBehaviour
             if (this.seasonTime <= 0f)
             {
                 ChangeSeason(Season.AUTUMN);
-                this.seasonTime = this.summerTime;
+                this.seasonTime = this.autumnTime;
             }
         }
         if (this.currentSeason == Season.AUTUMN)
@@ -129,7 +129,7 @@ public class WeatherManager : MonoBehaviour
             if (this.seasonTime <= 0f)
             {
                 ChangeSeason(Season.WINTER);
-                this.seasonTime = this.summerTime;
+                this.seasonTime = this.winterTime;
             }
         }
         if (this.currentSeason == Season.WINTER)
@@ -142,11 +142,11 @@ public class WeatherManager : MonoBehaviour
             if (this.seasonTime <= 0f)
             {
                 ChangeSeason(Season.SPRING);
-                this.seasonTime = this.summerTime;
+                this.seasonTime = this.springTime;
             }
         }
     }
-
+    
     private void LerpLightColor (Light light, Color c)
     {
         light.color = Color.Lerp(light.color, c, 0.2f * Time.deltaTime);
